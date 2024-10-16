dbf62265cca5eae89b52ca1fe7ac3c42986aee51
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2024-10-16 10:40:23 +0530

    changes

--- src/hooks/useUrlToGetPage.jsx
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-const useUrlToGetPage = () => {
-  const { pathname } = useLocation();
-
-  return pathname.split("/").pop();
-};
-
-export default useUrlToGetPage;
--- src/pages/Contact/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import Form from "../../components/ui/Form/Form";
-
-export default function Contact() {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
-}
