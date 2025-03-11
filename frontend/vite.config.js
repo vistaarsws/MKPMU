03337882b682b2b4d8d84d47e7ef108ad37ec4f2
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2025-03-11 18:02:23 +0530

    changes vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
     historyApiFallback: true,
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; img-src * data: blob:; script-src 'self' https://cdnjs.cloudflare.com https://res.cloudinary.com; ",
+    },
   },
   plugins: [react()],
 });
